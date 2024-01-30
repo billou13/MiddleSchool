1dde824849751e99a7c52c69aeb7b1f98c1a66fc
billou13 <billou13@gmail.com>
2024-01-30 21:50:42 +0100

    implement demo

--- MiddleSchool.ConsoleApp/Program.cs
+++ MiddleSchool.ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+
+switch(args[0])
+{
+    case "embed":
+        var image = SteganographyHelper.embedText(File.ReadAllText(args[1]), new Bitmap(@"Images\Bordeaux.png"));
+        image.Save($"{Path.GetFileNameWithoutExtension(args[1])}.png");
+        Console.WriteLine("Image créée !");
+        break;
+    case "extract":
+        string text = SteganographyHelper.extractText(new Bitmap(args[1]));
+        File.WriteAllText($"{Path.GetFileNameWithoutExtension(args[1])}.txt", text);
+        Console.WriteLine("Texte récupéré !");
+        break;
+}
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Code de démonstration
 
 Ce projet permet de présenter un code fonctionnel en seulement quelques lignes de code.
+
+## Images
+
+### Première image
+![Bordeaux](MiddleSchool.ConsoleApp/Images/Bordeaux.png)
+
+### Seconde image
+![Bordeaux](MiddleSchool.ConsoleApp/Images/Bordeaux-Large.png)
+
+## Remerciements
+Merci à:
+- [Molière - Le Malade imaginaire](https://fr.wikisource.org/wiki/Le_Malade_imaginaire)
+- [Jonathan Rieder](https://pixabay.com/fr/users/jonathanrieder-15553987)
+- [Steganography: Simple Implementation in C#](https://www.codeproject.com/Tips/635715/Steganography-Simple-Implementation-in-Csharp)
